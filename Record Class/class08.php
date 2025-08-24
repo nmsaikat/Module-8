e91282db99785653544e69aeac4ca37842d27bb3
nmsaikat <nmsaikat.cf@hmail.com>
2025-08-24 21:55:37 +0600

    persone OOP code update

--- Record Class/class08.php
+++ Record Class/class08.php
@@ -42,5 +42,33 @@ $c2 = new calculation();
 $c2->a = 50;
 $c2->b = 35;
 
-echo "Sum Value of C1: " . $c1->sum() . "\n";
-echo "Sub Value of C2: " . $c2->sub() . "\n";
+// echo "Sum Value of C1: " . $c1->sum() . "\n";
+// echo "Sub Value of C2: " . $c2->sub() . "\n";
+
+
+
+
+class person{
+    public $name;
+    public $age;
+
+    function __construct($name = "No Name", $age = 0){
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function show(){
+        echo $this->name . " - " . $this->age . "\n";
+    }
+}
+
+$p1 = new person();
+$p2 = new person("Yesmin", 15);
+$p3 = new person("Sony", 20);
+
+// $p1->name = "NM Saikat";
+// $p1->age = 26;
+
+$p1->show();
+$p2->show();
+$p3->show();
