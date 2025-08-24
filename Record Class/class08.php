a049913172986d9c3c68a1f09981dc638de63979
nmsaikat <nmsaikat.cf@hmail.com>
2025-08-24 21:33:12 +0600

    Class Some Code Add

--- Record Class/class08.php
+++ Record Class/class08.php
@@ -1,20 +1,46 @@
 <?php
 
-class man{
-    public $name;
-    public $age;
-    public $address;
+// class man{
+//     public $name;
+//     public $age;
+//     public $address;
 
-    function info (){
-        return 'Nebs IT';
+//     function info (){
+//         return 'Nebs IT';
+//     }
+// }
+
+
+// $saikat = new man();
+// $saikat->name = "NM Saikat\n";
+// echo $saikat->name;
+
+// $saki = new man();
+// $saki->name = "Saki";
+// echo $saki->name;
+
+class calculation{
+    public $a, $b, $c;
+
+    function sum(){
+        $this->c = $this->a + $this->b;
+        return $this->c;
+    }
+    function sub(){
+        $this->c = $this->a - $this->b;
+        return $this->c;
     }
 }
 
+$c1 = new calculation();
 
-$saikat = new man();
-$saikat->name = "NM Saikat\n";
-echo $saikat->name;
+$c1->a = 20;
+$c1->b = 10;
 
-$saki = new man();
-$saki->name = "Saki";
-echo $saki->name;
+$c2 = new calculation();
+
+$c2->a = 50;
+$c2->b = 35;
+
+echo "Sum Value of C1: " . $c1->sum() . "\n";
+echo "Sub Value of C2: " . $c2->sub() . "\n";
