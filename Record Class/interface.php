e0ab3159b8b689c2a0674b40e65d05fc90913f23
nmsaikat <nmsaikat.cf@gmail.com>
2025-09-01 09:10:11 +0600

    php interface some code add

---
+++ Record Class/interface.php
@@ -0,0 +1,23 @@
+<?php
+interface parent1 {
+    function calc($a, $b);
+}
+
+interface parent2 {
+    function sub($c, $d);
+}
+
+class childClass implements parent1, parent2{
+    public function calc($a, $b){
+        echo $a + $b . "\n";
+    }
+
+    public function sub($c, $d){
+        echo $c - $d . "\n";
+    }
+}
+
+$test = new childClass();
+$test->calc(50,20);
+
+$test->sub(50,20);
