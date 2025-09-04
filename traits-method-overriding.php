ecf6d0750fae26b0ddcc050225082a0db821870d
nmsaikat <nmsaikat.cf@gmail.com>
2025-09-04 08:22:33 +0600

    traits method overriding

--- traits-method-overriding.php
+++ traits-method-overriding.php
@@ -1,1 +1,23 @@
 <?php
+trait hello {
+    public function sayHello(){
+        echo "Hello Form hello Trait\n";
+    }
+}
+
+trait Hi {
+    public function sayHello(){
+        echo "Hello Form Hi Trait\n";
+    }
+}
+
+class Base {
+    use hello, Hi{
+        hello::sayHello insteadof Hi;
+        Hi::sayHello as sayHi;
+    }
+}
+
+$test = new Base();
+$test->sayHello();
+$test->sayHi();
